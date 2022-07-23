b3cd1642cb43b71f8e7c438b223dad8bfb8ccb0f
Xu Shaohua <shaohua@biofan.org>
2022-07-23 12:10:44 +0800

    Add sysinfo/

---
+++ sysinfo/src/controllers/main_controller.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU General Public License
+// that can be found in the LICENSE file.
+
+#ifndef SYSINFO_SRC_CONTROLLERS_MAIN_CONTROLLER_H_
+#define SYSINFO_SRC_CONTROLLERS_MAIN_CONTROLLER_H_
+
+class MainController {
+
+};
+
+#endif //SYSINFO_SRC_CONTROLLERS_MAIN_CONTROLLER_H_
---
+++ sysinfo/src/controllers/shell.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU General Public License
+// that can be found in the LICENSE file.
+
+#ifndef SYSINFO_SRC_CONTROLLERS_SHELL_H_
+#define SYSINFO_SRC_CONTROLLERS_SHELL_H_
+
+class Shell {
+
+};
+
+#endif //SYSINFO_SRC_CONTROLLERS_SHELL_H_
