fecd825dbe7d998d1d5d72f6d421fd5fe725e6c9
Xu Shaohua <shaohua@biofan.org>
2022-07-23 20:04:39 +0800

    sysinfo: Add process util

---
+++ sysinfo/src/base/process.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#ifndef SYSINFO_SRC_BASE_PROCESS_H_
+#define SYSINFO_SRC_BASE_PROCESS_H_
+
+#include <QString>
+#include <QStringList>
+
+namespace sysinfo {
+
+bool getCommandOutput(const QString& cmd, QString& output);
+bool getCommandOutput(const QString& cmd, const QStringList& args, QString& output);
+
+}  // namespace sysinfo
+
+#endif  // SYSINFO_SRC_BASE_PROCESS_H_
