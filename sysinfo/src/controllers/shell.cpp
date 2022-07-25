6d66c8d4dffe748ee196f8650ca6c3effd8e9494
Xu Shaohua <shaohua@biofan.org>
2022-07-25 18:26:41 +0800

    sysinfo: Add command line options

--- sysinfo/src/controllers/shell.cpp
+++ sysinfo/src/controllers/shell.cpp
@@ -5,13 +5,46 @@
 #include "controllers/shell.h"
 
 #include <QCoreApplication>
+#include <QCommandLineOption>
+#include <QCommandLineParser>
 #include <Logger.h>
 #include <ConsoleAppender.h>
 
 #include "config/config.h"
-#include "controllers/main_controller.h"
+#include "modules/computer/computer.h"
 
 namespace sysinfo {
+namespace {
+
+void readCommandLine() {
+  QCommandLineParser parser;
+  parser.addVersionOption();
+  parser.addHelpOption();
+  QCommandLineOption computer_opt("computer", "Fetch computer info");
+  parser.addOption(computer_opt);
+  QCommandLineOption devices_opt("devices", "Fetch hardware devices info");
+  parser.addOption(devices_opt);
+  QCommandLineOption network_opt("network", "Fetch network related info");
+  parser.addOption(network_opt);
+
+  parser.process(QCoreApplication::arguments());
+
+  if (parser.isSet(computer_opt)) {
+    computer::ComputerInfo info;
+    const bool ok = computer::getComputerInfo(info);
+    const QByteArray bytes = computer::dumpToJson(info);
+    printf("%s", bytes.toStdString().c_str());
+    return;
+  }
+  if (parser.isSet(devices_opt)) {
+    return;
+  }
+  if (parser.isSet(network_opt)) {
+    return;
+  }
+}
+
+}  // namespace
 
 int initShell(int argc, char** argv) {
   QCoreApplication app(argc, argv);
@@ -19,10 +52,9 @@ int initShell(int argc, char** argv) {
   QCoreApplication::setApplicationVersion(kAppVersion);
   cuteLogger->registerAppender(new ConsoleAppender);
 
-  QScopedPointer<MainController> controller(new MainController());
-  controller->init();
+  readCommandLine();
 
-  return QCoreApplication::exec();
+  return 0;
 }
 
 }  // namespace sysinfo
